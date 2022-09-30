f6855ee20339008f25d7e4cfbcf398f7f94f457f
Marty42780 <59181926+Marty42780@users.noreply.github.com>
2022-09-30 07:49:49 +0000

    changes of 30/09/2022 - updated files names - added .gitignore - grew on 03/Activite-1

---
+++ 03-Donnees_structurees-1/Activite-1_Les_Listes/Les_Listes.py
@@ -0,0 +1,40 @@
+def INSERER(L, x, i): 
+    if L[0] == (L.__len__()-1) or (i-i > L[0]):
+        print("’La liste est déjà pleine ou alors le rang n’est pas correct !")
+        return False
+    else:
+        for k in range(L[0]+1, i, -1):
+            L[k] = L[k-1]
+        L[i] = x
+    L[0] = L[0]+1
+    return True
+
+def SUPPRIMER(L, i):
+    if (L[0] != 0) and (i <= L[0]):
+        for k in range(i, L[0]):
+            L[k] = L[k+1]
+        L[0] = L[0]-1
+        return True
+    else:
+        print("La liste est déjà vide ou alors le rang n’est pas correct !")
+        return False
+
+Nb_Element = 5
+Liste=[None]*(Nb_Element+1)
+print(Liste)
+Liste[0] = 0
+print(Liste)
+INSERER(Liste, 'N', 1)
+print(Liste)
+INSERER(Liste, 'I', 2)
+print(Liste)
+INSERER(Liste, 'I', 3)
+print(Liste)
+SUPPRIMER(Liste, 2)
+print(Liste)
+INSERER(Liste,'S',2)
+print(Liste)
+INSERER(Liste, 2020, 4)
+print(Liste)
+INSERER(Liste, 2021, 5)
+print(Liste)
