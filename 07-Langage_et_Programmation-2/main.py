9bbef5e36f2ef3c3aaf4236f90a2108900b6fdce
Marty42780 <59181926+Marty42780@users.noreply.github.com>
2023-01-16 14:50:04 +0000

    changes of 16/01/2023 - added 07/Activite-1

---
+++ 07-Langage_et_Programmation-2/main.py
@@ -0,0 +1,15 @@
+from math import sqrt
+
+class Point(object):
+    "Définition d'un point Mathématique"
+
+def affiche_point(p):
+    print("Coordonnées :", str(p.x), ";", str(p.y))
+
+def distance(p1, p2):
+    return math.sqrt((()))
+
+p5, p6 = (Point() for i in range(2))
+p5.x, p5.y, p6.x, p6.y = 12.3, 5.7, 6.2, 9.1
+affiche_point(p6)
+
