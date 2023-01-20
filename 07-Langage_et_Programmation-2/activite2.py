e8f55487bbae5d015bfc4dc29c1db3a71284b5a7
Marty42780 <59181926+Marty42780@users.noreply.github.com>
2023-01-20 08:50:08 +0000

    Changes of 20/01/2023 - Activite 3 of 07

--- 07-Langage_et_Programmation-2/activite2.py
+++ 07-Langage_et_Programmation-2/activite2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import random
 
 
@@ -14,11 +15,16 @@ class Personnage:
         self.vie -= random.randint(1, 3)
 
 
-Bilbo = Personnage(20)
-Gollum = Personnage(30)
-
-Bilbo.donneEtat()
-Gollum.perdVie()
-Gollum.donneEtat()
-Bilbo.perdVie()
-Bilbo.donneEtat()
+def game():
+    Bilbo = Personnage(20)
+    Gollum = Personnage(30)
+    while Bilbo.donneEtat() > 0 and Gollum.donneEtat() > 0:
+        Bilbo.perdVie()
+        Gollum.perdVie()
+    if Bilbo.donneEtat() <= 0 and Gollum.donneEtat() > 0:
+        message = f"Gollum est vainqueur, il lui reste encore {Gollum.donneEtat()} points alors que Bilbo est mort."
+    elif Gollum.donneEtat() <= 0 and Bilbo.donneEtat() > 0:
+        message = f"Bilbo est vainqueur, il lui reste encore {Bilbo.donneEtat()} points alors que Gollum est mort."
+    else:
+        message = f"Les deux combattants sont morts en m�me temps."
+    return message
---
+++ 07-Langage_et_Programmation-2/activite3.py
@@ -0,0 +1,2 @@
+class Time(object):
+    "DÃfinition d'objets temporels"
